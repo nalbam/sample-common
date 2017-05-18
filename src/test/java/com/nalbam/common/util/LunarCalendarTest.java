c0ed99ec4e4fa89923b57c3caa5cc51ebac5b4c3
nalbam <nalbam@nalbam-mac.local>
2017-05-19 01:32:27 +0900

    indent

--- src/test/java/com/nalbam/common/util/LunarCalendarTest.java
+++ src/test/java/com/nalbam/common/util/LunarCalendarTest.java
@@ -7,43 +7,40 @@ import org.junit.Test;
 
 public class LunarCalendarTest {
 
-	@Test
-	public void testConvert() {
-		String str1 = "1980-02-14";
-		String str2 = "1979-12-28";
+    @Test
+    public void testConvert() {
+        String str1 = "1980-02-14";
+        String str2 = "1979-12-28";
 
-		String var1 = LunarCalendar.toLunar(str1);
-		String var2 = LunarCalendar.toSolar(str2);
+        String var1 = LunarCalendar.toLunar(str1);
+        String var2 = LunarCalendar.toSolar(str2);
 
-		assertEquals(str1, var2);
-		assertEquals(str2, var1);
-	}
+        assertEquals(str1, var2);
+        assertEquals(str2, var1);
+    }
 
-	@Test
-	public void testNull() {
-		LunarCalendar lc = new LunarCalendar();
+    @Test
+    public void testNull() {
+        LunarCalendar lc = new LunarCalendar();
 
-		String str1 = null;
-		String str2 = null;
+        String var1 = LunarCalendar.toLunar(null);
+        String var2 = LunarCalendar.toSolar(null);
 
-		String var1 = LunarCalendar.toLunar(str1);
-		String var2 = LunarCalendar.toSolar(str2);
+        assertNotNull(lc);
+        assertEquals(var1, "");
+        assertEquals(var2, "");
+    }
 
-		assertNotNull(lc);
-		assertEquals(var1, "");
-		assertEquals(var2, "");
-	}
+    @Test
+    public void testLength() {
+        String str1 = "111";
+        String str2 = "222";
 
-	@Test
-	public void testLength() {
-		String str1 = "111";
-		String str2 = "222";
+        String var1 = LunarCalendar.toLunar(str1);
+        String var2 = LunarCalendar.toSolar(str2);
 
-		String var1 = LunarCalendar.toLunar(str1);
-		String var2 = LunarCalendar.toSolar(str2);
-
-		assertEquals(var1, "");
-		assertEquals(var2, "");
-	}
+        assertEquals(var1, "");
+        assertEquals(var2, "");
+    }
 
 }
