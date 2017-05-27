d27fff4603e2bfee8f4cf511ea6f9c41356e84df
nalbam-ya <jyyu@yanolja.com>
2017-05-28 01:50:10 +0900

    v

--- src/main/java/com/nalbam/common/util/VersionUtil.java
+++ src/main/java/com/nalbam/common/util/VersionUtil.java
@@ -3,18 +3,18 @@ package com.nalbam.common.util;
 public class VersionUtil {
 
     public static synchronized String getVersion(Class<?> c) {
-        String version = null;
+        String v = null;
         Package p = c.getPackage();
         if (p != null) {
-            version = p.getImplementationVersion();
-            if (version == null) {
-                version = p.getSpecificationVersion();
+            v = p.getImplementationVersion();
+            if (v == null) {
+                v = p.getSpecificationVersion();
             }
         }
-        if (version == null) {
-            version = "unknown";
+        if (v == null) {
+            v = "unknown";
         }
-        return version;
+        return v;
     }
 
 }
